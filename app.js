ee737226f3c16674f6e29a695d78062e86b61ed3
Yash Jain <124608942+yashYJ621@users.noreply.github.com>
2024-02-11 19:35:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,23 +7,6 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Data Blog!');
 });
 
-app.get('/post1', (req, res) => {
-  res.send('This is the first post about data analysis.');
-});
-
-app.get('/post2', (req, res) => {
-  res.send('This is the second post about machine learning techniques.');
-});
-
-app.get('/post3', (req, res) => {
-  res.send('This is the third post about data visualization.');
-});
-
-// Set up a catch-all route for other URLs
-app.get('*', (req, res) => {
-  res.send('Page not found.');
-});
-
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
 });
