deebf5cf0f2efe0d71ed2a62889a30acd576f53c
yashYJ621 <yashbiz621@gmail.com>
2024-02-11 19:24:41 -0500

    feat: new content

--- app.js
+++ app.js
@@ -2,7 +2,28 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// Set up routes for different blog posts
+app.get('/', (req, res) => {
+  res.send('Welcome to the Data Blog!');
+});
 
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
+app.get('/post1', (req, res) => {
+  res.send('This is the first post about data analysis.');
+});
+
+app.get('/post2', (req, res) => {
+  res.send('This is the second post about machine learning techniques.');
+});
+
+app.get('/post3', (req, res) => {
+  res.send('This is the third post about data visualization.');
+});
+
+// Set up a catch-all route for other URLs
+app.get('*', (req, res) => {
+  res.send('Page not found.');
+});
+
+app.listen(port, () => {
+  console.log(`App running on http://localhost:${port}`);
+});
